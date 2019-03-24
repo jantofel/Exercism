d6ae41401e34786b94752215015c4aedd27b6a02
Jan Tofel <jan.tofel@gmail.com>
2019-03-24 21:36:13 +1100

    Grains without using sumBy
    I cannot figure out how to make sumBy work with 'Result's, so here is a solution without it

--- fsharp/grains/Grains.fs
+++ fsharp/grains/Grains.fs
@@ -2,13 +2,15 @@
 
 let square (n: int): Result<uint64,string> = 
     match n with
-    | n when n < 0 -> Error "Cannot be negative "
-    | n when n >= 64 -> Error "Cannot be that big"
-    | _ -> Ok ( pown (uint64 2) n )
+    | i when i <= 0 || i > 64 -> Error "square must be between 1 and 64"
+    | _ -> Ok ( pown (uint64 2) (n-1) )
     
+
 let total: Result<uint64,string> = 
-    let res = uint64 0
-    let squares = List.map square [0..63]
-    let x = square 5
-    x.
-    List.sumBy (fun x -> x.ResultValue) squares
+    let rec mySum sequence (accumulator: uint64): Result<uint64,string>=
+        match sequence with
+        | [] -> Ok accumulator
+        | Error err :: _ -> Error err
+        | Ok n:: rest -> mySum rest (n+accumulator)
+    // Ideally, I'd want to use sumBy instead, but I don't know how
+    mySum (List.map square [1..64]) (uint64 0)
