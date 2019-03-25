354f5b55346a111f124081f0f7914f2a8a848a3f
Jan Tofel <jan.tofel@gmail.com>
2019-03-25 22:19:21 +1100

    a simple transcription

---
+++ fsharp/rna-transcription/RnaTranscription.fs
@@ -0,0 +1,12 @@
+﻿module RnaTranscription
+
+let toRna (dna: string): string = 
+    let transOne character = 
+        match character with
+        | 'A' -> 'U'
+        | 'T' -> 'A'
+        | 'C' -> 'G'
+        | 'G' -> 'C'
+        | _ -> '_'
+    String.map transOne dna
+    
