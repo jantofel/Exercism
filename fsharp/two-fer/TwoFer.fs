cd29d6e02d406da5e0785de7e00f2dfd8022bdc0
Jan Tofel <jan.tofel@gmail.com>
2019-06-09 08:24:24 +1000

    Initial solution for TwoFer

---
+++ fsharp/two-fer/TwoFer.fs
@@ -0,0 +1,8 @@
+﻿module TwoFer
+
+let twoFer (input: string option): string =
+    let otherParty = 
+        match input with
+        | None -> "you"
+        | Some name -> name
+    "One for " + otherParty + ", one for me."
