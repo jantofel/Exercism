e8e0d931cecc31fef52db4c991914aa3ed2b3403
Jan Tofel <jan.tofel@gmail.com>
2019-06-08 13:45:04 +1000

    an iterative solution to serve as a baseline

---
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,7 @@
+﻿module SumOfMultiples
+
+let sum (numbers: int list) (upperBound: int): int =
+    let whichDivisible n = ( List.map (fun x -> x<>0 && n%x = 0 ) numbers )
+    let allDivisible n = List.fold (||) false ( whichDivisible n )
+    let y = List.filter allDivisible [1..upperBound-1]
+    List.sum y
