ebc740513fce556bfd31624363842afa34e6c761
Jan Tofel <jan.tofel@gmail.com>
2019-06-08 13:49:08 +1000

    slightly more F#

--- fsharp/sum-of-multiples/SumOfMultiples.fs
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -1,7 +1,8 @@
 ﻿module SumOfMultiples
 
 let sum (numbers: int list) (upperBound: int): int =
-    let whichDivisible n = ( List.map (fun x -> x<>0 && n%x = 0 ) numbers )
+    let whichDivisible n = List.map (fun x -> x<>0 && n%x = 0 ) numbers
     let allDivisible n = List.fold (||) false ( whichDivisible n )
-    let y = List.filter allDivisible [1..upperBound-1]
-    List.sum y
+    [1..upperBound-1]
+    |> List.filter allDivisible
+    |> List.sum
