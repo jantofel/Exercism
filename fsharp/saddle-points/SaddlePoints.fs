d4084807b12dc5e87b20d3aaf3414e3db6eb97fe
Jan Tofel <jan.tofel@gmail.com>
2019-06-09 09:27:40 +1000

    minor clean up

--- fsharp/saddle-points/SaddlePoints.fs
+++ fsharp/saddle-points/SaddlePoints.fs
@@ -1,22 +1,26 @@
 module SaddlePoints
 
-let filteri (predicate:'T -> bool) (list: 'T list): int list =
-    let indexedList = List.mapi (fun idx elem -> (idx, elem)) list
-    let filteredIndexedList = List.filter (fun (idx, elem) -> predicate elem) indexedList
-    List.map (fun (idx, _elem) -> idx) filteredIndexedList
+let findAll (predicate:'T -> bool) (list: 'T list): int list =
+    list 
+    |> List.mapi (fun idx elem -> (idx, elem))
+    |> List.filter (fun (idx, elem) -> predicate elem)
+    |> List.map (fun (idx, _elem) -> idx)
 
 let rec product accumulator (list1: 'T list) (list2: 'U list): ('T * 'U) list =
     match list1 with
     | [] -> accumulator
     | val1 :: rest1 ->
-        let x = List.map (fun elem -> (val1,elem)) list2
+        let x = List.map ( fun elem -> (val1,elem) ) list2
         product ( accumulator @ x ) rest1 list2
-
+        
 let saddlePoints (matrix: int list list): (int*int) list =
-    if List.isEmpty matrix then []
+    if matrix = [[]] then []
     else
         let rowMaxima = List.map List.max matrix
-        let colMinima = List.map List.min (List.transpose matrix)
-        let rowIndices = filteri (fun elem -> List.contains elem colMinima ) rowMaxima
-        let colIndices = filteri (fun elem -> List.contains elem rowMaxima ) colMinima  
+        let colMinima = 
+            matrix 
+            |> List.transpose
+            |> List.map List.min
+        let rowIndices = findAll ( fun elem -> List.contains elem colMinima ) rowMaxima
+        let colIndices = findAll ( fun elem -> List.contains elem rowMaxima ) colMinima  
         product [] rowIndices colIndices
