56b2705f7a49f21e6c21f1dca344daee83324076
Jan Tofel <jan.tofel@gmail.com>
2019-06-14 19:56:17 +1000

    Simplifying

--- fsharp/two-fer/TwoFer.fs
+++ fsharp/two-fer/TwoFer.fs
@@ -1,5 +1,5 @@
 ﻿module TwoFer
 
 let twoFer (input: string option): string =
-    let otherParty = Option.defaultValue "you" input
-    sprintf "One for %s, one for me." otherParty
+    Option.defaultValue "you" input
+    |> sprintf "One for %s, one for me."
