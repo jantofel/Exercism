ac08f5c6e5794156be73b12b69e6fa94ddebdc81
Jan Tofel <jan.tofel@gmail.com>
2019-07-21 16:31:09 +1000

    Initial version
    I needed to supply a dummy default value to satisfy the type

---
+++ fsharp/binary-search-tree/BinarySearchTree.fs
@@ -0,0 +1,30 @@
+﻿module BinarySearchTree
+
+type TreeNode<'a> = { left: Option<TreeNode<'a>>; right: Option<TreeNode<'a>>; data:'a }
+
+let left node = node.left
+    //node |> Option.bind (fun n -> n.left)
+
+let right node = node.right
+    //node |> Option.bind (fun n -> n.right)
+
+let data node = node.data
+
+let rec create' (items:list<int>): TreeNode<int> option = 
+    match items with
+    | [] -> None
+    | item :: items' ->
+        let left = create' ( List.filter (fun x -> x <= item) items' )
+        let right = create' ( List.filter (fun x -> x > item) items' )
+        Some { left = left; right = right; data = item }
+
+let create (items:list<int>): TreeNode<int> = 
+    create' items |> Option.defaultValue { left = None; right = None; data = -1}
+        
+let rec sortedData' node = 
+    match node with
+    | None -> []
+    | Some { left=l; right=r; data=d } -> List.append (sortedData' l) (d :: sortedData' r)
+
+let sortedData node =
+    node |> Option.Some |> sortedData'
